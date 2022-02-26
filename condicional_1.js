126eb3b1d224cbdcdaf3ba88eec98e28db254a7c
aristianes <71615848+aristianes@users.noreply.github.com>
2022-02-26 19:34:39 -0300

    Add files via upload

---
+++ condicional_1.js
@@ -0,0 +1,37 @@
+/*
+2 - Escreva um progrma que de informações sobre o peso
+Se o peso for menor que 27 kg escreva "vc está abaixo do peso"
+Se o peso for maior que 40 kg escreva "Vc está acima do peso"
+
+
+*/
+
+let peso;
+
+peso = 60
+
+if (peso < 27) {
+    console.log("Você está a baixo do peso");
+}
+if (peso > 40) {
+    console.log(" Você está a cima do peso");
+}
+
+
+
+/*
+3- Escreva um programa para ler 2 valores (considere que não serão
+informados valores iguais) e escrever o maior deles.
+*/
+
+let valor_a;
+let valor_b;
+
+valor_a = 80;
+valor_b = 60;
+
+if (valor_a < valor_b) {
+    console.log("O valor A é o menor");
+} else {
+    console.log("O valor A é maior");
+}
---
+++ condicional_3.js
@@ -0,0 +1,29 @@
+/*
+Receber 4 notas
+calcular a media
+se a media > 9 escrever " Aprovado com louvor + nota"
+se a media > 7 e < 9  escrever " aprovado"
+
+*/
+
+let nota_1;
+let nota_2;
+let nota_3;
+let nota_4;
+let media;
+
+nota_1 = 6;
+nota_2 = 5;
+nota_3 = 5;
+nota_4 = 7;
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
+
+if (media > 9) {
+    console.log(" Aprovado com louvor " + media);
+}
+if (media >= 7 && media < 9) {
+    console.log("Aprovado " + media);
+} else {
+    console.log(" Reprovado " + media);
+
+}
